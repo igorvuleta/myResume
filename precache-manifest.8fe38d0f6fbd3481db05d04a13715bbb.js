681c4a0351f1a306c177ee4bc30cdb4e000de218
igorvuleta <ivuleta@gmail.com>
2018-11-24 01:17:24 +0100

    Updates

---
+++ precache-manifest.8fe38d0f6fbd3481db05d04a13715bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9d64ced8a580540e59b",
+    "url": "/myResume/static/js/runtime~main.e9d64ced.js"
+  },
+  {
+    "revision": "2bdec9ee53c80ebb7ddf",
+    "url": "/myResume/static/js/main.2bdec9ee.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/myResume/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "2bdec9ee53c80ebb7ddf",
+    "url": "/myResume/static/css/main.1f696944.chunk.css"
+  },
+  {
+    "revision": "00b1693f0d9fb2e40cb73a0cea493b26",
+    "url": "/myResume/index.html"
+  }
+];
