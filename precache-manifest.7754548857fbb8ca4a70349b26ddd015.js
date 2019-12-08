cdd8891675708676ee52afd5849042d8446b9f77
igorvuleta <ivuleta@gmail.com>
2019-12-08 22:10:22 +0100

    Updates

--- precache-manifest.7754548857fbb8ca4a70349b26ddd015.js
+++ precache-manifest.7754548857fbb8ca4a70349b26ddd015.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/myResume/static/js/runtime~main.4fcd64c1.js"
   },
   {
-    "revision": "d3bb22db2c22dda7400c",
-    "url": "/myResume/static/js/main.6b9c11d1.chunk.js"
+    "revision": "99b6c5f70272c1242783",
+    "url": "/myResume/static/js/main.3123e085.chunk.js"
   },
   {
     "revision": "ae0b72b89335021e0997",
     "url": "/myResume/static/js/2.4952e05c.chunk.js"
   },
   {
-    "revision": "d3bb22db2c22dda7400c",
+    "revision": "99b6c5f70272c1242783",
     "url": "/myResume/static/css/main.591c85cc.chunk.css"
   },
   {
-    "revision": "4fb45183f5d937057e24025811714ff9",
+    "revision": "15c8a37712652f913dd6f0d077c82bd4",
     "url": "/myResume/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myResume/precache-manifest.91c8bf263df56f9e0e8f8b1398a2a079.js"
+  "/myResume/precache-manifest.7754548857fbb8ca4a70349b26ddd015.js"
 );
 
 workbox.clientsClaim();
